1ffcfceaf6ddf1c1c3295746f8238ba013dca4f2
Samir Marin <marin.samir@gmail.com>
2021-01-14 15:15:46 -0800

    Create ec2 instance using

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0be2609ba883822ec"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "terraform-example"
+  }
+}
